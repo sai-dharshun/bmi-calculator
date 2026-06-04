67aa320a63bc316eb8d5296b38fc57bf5490c998
Sai Dharshun <sai.dharshun@perficient.com>
2026-06-04 11:16:34 +0530

    Adding findBMI

--- bmi.py
+++ bmi.py
@@ -1,3 +1,8 @@
+def findBMI(height, weight):
+    bmi = weight / (height ** 2)
+    return bmi
+
+
 
 height = float(input("Enter your height in meters: "))
 weight = float(input("Enter your weight in kilograms: "))
